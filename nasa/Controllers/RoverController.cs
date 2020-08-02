a73c84bc0658fe05c6c5c0547b2683384c85717a
Eric Day <eric.ch.day@gmail.com>
2020-08-02 13:45:54 -0600

    Basic grid layout

---
+++ nasa/Controllers/RoverController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using nasa.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace nasa.Controllers
+{
+    public class RoverController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        //public string Index()
+        {
+            int _grid = 0;
+
+            _grid = getGridSize(_grid);
+
+            ViewData["GridSize"] = _grid;
+            RoverMachine[] deployedRovers = getRovers();
+            //ViewData["Rovers"] = getRovers();
+            return View(deployedRovers);
+        }
+
+        private int getGridSize(int _grid)
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5002/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("grid");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    _grid = Int32.Parse(readTask.Result);
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+
+            return _grid;
+        }
+
+        private RoverMachine[] getRovers()
+        {
+            RoverMachine[] _rovers = new RoverMachine[0];
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+
+                client.BaseAddress = new Uri("http://localhost:5002/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("rovers");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    string json = readTask.Result;
+
+                    _rovers = JsonConvert.DeserializeObject<RoverMachine[]>(json);
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+
+            return _rovers;
+        }
+
+    }
+}
---
+++ nasa/Models/Rover.cs
@@ -0,0 +1,21 @@
+﻿namespace nasa.Models
+{
+    public class RoverMachine
+    {
+        public int Id { get; set; }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public char Heading { get; set; }
+
+    }   
+
+    public class RoverMovements
+    {
+        public string move { get; set; }
+    }
+
+    public class Grid
+    {
+        public int MaxSize { get; set; }
+    }
+}
